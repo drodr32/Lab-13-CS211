2d16ddefd2600b517f313b29f0f315d5916c7504
Diana (UIC Info: email: drodr32@uic.edu) Rodriguez <118220564+drodr32@users.noreply.github.com>
2022-11-17 17:10:14 +0000

    Added Print statements for ID's and what we learned

---
+++ .ccls-cache/@home@runner@Lab-13-CS211/211-f22-collab-github@collab.c
@@ -0,0 +1,40 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void jali24_drod32(){
+  printf("Diana Rodriguez\n");
+  printf("Junaid Ali\n");
+  printf("Something we learned :) \n");
+  printf("We learned that in order to push the branch we needed to generate a token and this would allow us to push the branch to our remote GitHub Repository.");
+  printf("\nTrouble we had :( \n");
+  printf("We didn't know that we needed to generate a token to access the clone that we had created from Professor Kidane's Repository");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    
+    return 0;
+}
